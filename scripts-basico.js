ad9f33806b707c650174ec9099050d3b4e51964c
Titicagens <willianweber123@gmail.com>
2024-05-08 20:28:46 -0300

    primeiro commit

---
+++ scripts-basico.js
@@ -0,0 +1,30 @@
+// O vento DOM é adicionado quando todo o HTML foi completamente carregado
+
+document.addEventListener('DOMContentLoaded', function () {
+
+        alert(123)
+        //Tipo de dados
+        //String: Sequencia de caracter
+        //Number: Números inteiros ou pontos flutuantes(10.4)
+        //BooLean: Retorna dois valores, true ou false
+        //Object: Estrutura de dados que pode armazenar múltiplos valores
+        //Null: Quando está vazio
+        //Array: Objeto especial ordenado
+        //Function: Bloco de código reutilizável
+
+        //CONST É usado para declarar uma variável cujo valor é permanente
+            const test = 50;
+        
+            //LET é usadp ára declarar variáveis podem ser retribuídos 
+            let B = 0;
+
+        console.log("Silvio")
+
+
+
+})
+
+
+
+
+
